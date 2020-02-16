3452a36dc72764495d034c9f81baf879f65296c2
andrecoronado <andcoronad@hotmail.com>
2020-02-16 18:31:16 -0500

    production

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const userRouter = require('./routers/user-router')
 const messageRouter = require('./routers/message-router')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
@@ -13,12 +13,31 @@ app.use(userRouter)
 app.use(messageRouter)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+  
+if(process.env.ENVIRON==='PROD'){
+  const httpsServer = https.createServer({
+    key: fs.readFileSync('/home/admin/conf/web/ssl.andrecoronado.com.key'),
+    cert: fs.readFileSync('/home/admin/conf/web/ssl.andrecoronado.com.crt'),
+  }, app)
 
-app.get('/api', function (req, res) {
+  app.get('/api', function (req, res) {
+    res.send('<h1>Welcome to WebChat | by andreCoronado</h1>')
+  }) 
+  
+  app.get('*', function (req, res) {
+  res.redirect('https://andrecoronado.com/webchat/')
+  })
+
+  httpsServer.listen(port, () => {
+  console.log('HTTPS Server running on port ' + port)
+  });  
+}
+else{
+  app.get('/api', function (req, res) {
     res.send('<h1>Welcome to WebChat | by andreCoronado</h1>')
   })
-
-app.listen(port, () => {
+  app.listen(port, () => {
     console.log('Server is up on port ' + port)
   })
+}
 
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -41,7 +41,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, 'WebChatJSAndreCoronado')
+    const token = jwt.sign({ _id: user._id.toString() }, process.env.TOKENSECRET)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
